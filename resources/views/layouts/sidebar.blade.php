ee0287f762f659ef9d911b2fa79cf6f4bfa1dfbd
Andres Rincon <zero389@gmail.com>
2016-11-21 14:05:13 -0500

    'suscriptores'

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -10,7 +10,7 @@
             </ul>
         </li>
         <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Suscriptor <span class="caret"></span></a>
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Suscriptores <span class="caret"></span></a>
             <ul class="dropdown-menu forAnimate" role="menu">
                 <li><a href="{{ url('/suscriptores') }}">Listar Suscriptores</a></li>
                 <li class="divider"></li>
