2a677c2e30f6b670609d3da7b9621160a80e184c
Andres Rincon <zero389@gmail.com>
2016-11-23 08:32:24 -0500

    'wip'

--- public/js/rescueruao.js
+++ public/js/rescueruao.js
@@ -8,8 +8,10 @@ function initMap() {
   var centroMapa = {lat: 3.454956, lng: -76.5183073};
   var map = new google.maps.Map(document.getElementById('mapa'), {
     zoom: 12,
-    center: centroMapa
+    center: centroMapa,
+    mapTypeId: 'roadmap'
   });
+  map.setTilt(45);
   //Obtener informacion de latitud y longitud
   $.get( '/getvehicles', function( vehiculos ) {
     for (i = 1;  i <= vehiculos.cantidad; i++){
@@ -20,12 +22,14 @@ function initMap() {
         ubicacion.lat = parseFloat(ubicacion.lat, 10);
         ubicacion.lng = parseFloat(ubicacion.lng, 10);
         var uluru = {lat: ubicacion.lat, lng: ubicacion.lng};
+        // Multiple Markers
+
         var marker = new google.maps.Marker({
           position: uluru,
           label: ubicacion.placa,
           map: map
         });
-        console.log(marker);
+
       });
     }
   });
