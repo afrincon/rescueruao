f91696daaaa6905738594424059bfac152ea5d82
Andres Rincon <zero389@gmail.com>
2016-11-23 14:25:12 -0500

    wip add column estado to ubicactions

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -26,5 +26,20 @@ class UserTableSeeder extends Seeder
           'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
           'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
       ]);
+
+        DB::table('users')->insert([
+            'first_name'  =>  'Giovanni',
+            'last_name' =>  'Cortes',
+            'gender'  =>  'Masculino',
+            'name'  => 'Administrador',
+            'email'  =>  'gcortes@rescueruao.io',
+            'password' => bcrypt('Rescuer123*'),
+            'id_rol'  =>  '1',
+            'address' =>  'Avenida Siempre Viva 123',
+            'phone' =>  '8930303',
+            'estado'  =>  'Activo',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
     }
 }
