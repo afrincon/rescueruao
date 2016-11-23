ca784d8c6dd8b25afec0c6cde644fd47b8ed585e
Andres Rincon <zero389@gmail.com>
2016-11-22 23:37:21 -0500

    agregar zoom a mapa

--- public/js/rescueruao.js
+++ public/js/rescueruao.js
@@ -1,18 +1,18 @@
 window.jQuery(function() {
   // Handler for .ready() called.
-	console.log('dom is ready');
+	// console.log('dom is ready');
 	initMap();
 });
 
 function initMap() {
   var uluru = {lat: 3.454956, lng: -76.5183073};
   var map = new google.maps.Map(document.getElementById('mapa'), {
-    zoom: 4,
+    zoom: 12,
     center: uluru
   });
   var marker = new google.maps.Marker({
     position: uluru,
     map: map
   });
-	console.log('ran init map');
+	// console.log('ran init map');
 }
