bb5d6be09322526c06f3548329d0bd9be67748c4
Andres Rincon <zero389@gmail.com>
2016-11-22 23:05:58 -0500

    Agregar gmaps en vue

--- resources/assets/js/components/Example.vue
+++ resources/assets/js/components/Example.vue
@@ -1,23 +1,32 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Example Component</div>
-
-                    <div class="panel-body">
-                        I'm an example component!
-                    </div>
-                </div>
+            <div class="col-md-12">
+                <div id="mapa"></div>
             </div>
         </div>
     </div>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDZTMV320Z8FMSdJN4dc29uzzqKIa2GWA0&callback=initMap">
+    </script>
 </template>
 
 <script>
     export default {
         mounted() {
             console.log('Component ready.')
+        }
+
+        function initMap() {
+          var uluru = {lat: -25.363, lng: 131.044};
+          var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 4,
+            center: uluru
+          });
+          var marker = new google.maps.Marker({
+            position: uluru,
+            map: map
+          });
         }
     }
 </script>
