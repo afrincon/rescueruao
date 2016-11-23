5b149d1df35a0e9e42bb7ea2ad90d3d272396958
Andres Rincon <zero389@gmail.com>
2016-11-23 09:13:49 -0500

    'wip'

--- public/js/rescueruao.js
+++ public/js/rescueruao.js
@@ -6,16 +6,12 @@ window.jQuery(function() {
 
 function initMap() {
   var centroMapa = {lat: 3.454956, lng: -76.5183073};
-  var bounds = new google.maps.LatLngBounds();
   var map = new google.maps.Map(document.getElementById('mapa'), {
     zoom: 12,
     center: centroMapa,
     mapTypeId: 'roadmap'
   });
   map.setTilt(45);
-
-  var markers = new Array();
-  var infoWindowContent = new Array();
 
   //Obtener informacion de latitud y longitud
   $.get( '/getvehicles', function( vehiculos ) {
@@ -26,24 +22,13 @@ function initMap() {
         // console.log('cargada data', ubicacion);
         ubicacion.lat = parseFloat(ubicacion.lat, 10);
         ubicacion.lng = parseFloat(ubicacion.lng, 10);
-        /*var uluru = {lat: ubicacion.lat, lng: ubicacion.lng};
+        var uluru = {lat: ubicacion.lat, lng: ubicacion.lng};
 
         var marker = new google.maps.Marker({
           position: uluru,
           label: ubicacion.placa,
           map: map
-        });*/
-        markers.push([ubicacion.placa, ubicacion.lat, ubicacion.lng]);
-      });
-    }
-    for( i = 0; i < markers.length; i++ ) {
-      var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
-      console.log(position);
-      bounds.extend(position);
-      marker = new google.maps.Marker({
-        position: position,
-        map: map,
-        title: markers[i][0]
+        });
       });
     }
   });
